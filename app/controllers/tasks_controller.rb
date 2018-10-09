245cde0d32f4060f322d86c7599742e0adb18d7a
Hengyu Lou <hylou34@gmail.com>
2018-10-08 22:30:40 -0700

    Update task show layout

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -82,6 +82,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:taskname, :deadline, :importance, :category, :user_id)
+      params.require(:task).permit(:taskname, :deadline, :importance, :category, :user_id, :description)
     end
 end
