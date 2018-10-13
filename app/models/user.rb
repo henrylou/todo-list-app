8b5a3054965dab7791e603cf9bc99caed61bc23b
Hengyu Lou <hylou34@gmail.com>
2018-10-13 01:30:32 -0700

    Update new user and session pages layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :recoverable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
   has_many :tasks
 end
